6d6f112e21601c7ce72af656c47ae0a490c51e2d
Yomar Miranda <mirandayomar@gmail.com>
2018-09-23 00:34:57 -0500

    Create Info Interface.

---
+++ src/app/interfaces/info.interface.ts
@@ -0,0 +1,11 @@
+export interface Info {
+    title?: string;
+    email?: string;
+    short_name?: string;
+    website?: string;
+    facebook?: string;
+    twitter?: string;
+    instagram?: string;
+    tumblr?: string;
+    team?: any[];
+}
